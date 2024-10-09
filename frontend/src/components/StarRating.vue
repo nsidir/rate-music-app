5c975cc208d08a31b052188f85faac6f39d25c18
Nikos Sidiropoulos <nicksidiropoulos56@gmail.com>
2024-10-10 02:11:53 +0300

    Minor StarRating component changes

--- frontend/src/components/StarRating.vue
+++ frontend/src/components/StarRating.vue
@@ -1,21 +1,16 @@
 <template>
-    <div class="rating_wrapper">
-      <div class="rating_stars" @mouseleave="resetHoveredRating" @click="handleStarsClick">
-        <div
-          class="star"
-          v-for="n in 5"
-          :key="n"
-          :class="{'hovered': n <= hoveredRating || n <= clickedRating}"
-          @mouseover="hoverRating(n)"
-          @click.stop="clickRating(n)"
-        ></div>
-      </div>
+  <div class="rating_wrapper">
+    <div class="rating_stars" @mouseleave="resetHoveredRating" @click="handleStarsClick">
+      <div class="star" v-for="n in 5" :key="n" :class="{'hovered': n <= hoveredRating || n <= clickedRating}"
+        @mouseover="hoverRating(n)" @click.stop="clickRating(n)"></div>
+
       <!-- Display the rating to the right, showing "-/5" when no rating is selected -->
       <div class="rating_display">
         {{ clickedRating > 0 ? clickedRating : '-' }} / 5
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <script lang="ts">
   import { defineComponent, ref } from 'vue';
@@ -74,6 +69,7 @@
     height: 16px;
     background: url('https://e.snmc.io/2.5/img/star_sprite_4.png') no-repeat top left;
     margin-right: 2px;
+    margin-top: 5px;
     cursor: pointer;
   }
   .star.hovered:nth-child(1) { background-position: 0 -198px; }
